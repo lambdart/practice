c3957887b8b2885718812d28bac6bee57da5958a
lambdart <lambdart@protonmail.com>
2022-07-23 23:00:18 -0300

    Add run-length-encoding solution

---
+++ clojure/src/run-length-encoding.clj
@@ -0,0 +1,16 @@
+(ns run-length-encoding
+  (:require [clojure.string :as str :refer [join blank?]]
+            [criterium.core :refer [quick-bench with-progress-reporting]]))
+
+(defn run-length-encode [s]
+  (apply str
+         (remove #{1}
+                 (mapcat (juxt count first)
+                         (partition-by identity s)))))
+
+(defn run-length-decode
+  [s]
+  (apply str
+         (mapcat (fn [[_ n c]]
+                   (repeat (if (= n "") 1 (Integer. n)) c))
+                 (re-seq #"(\d*)(\D)" s))))
