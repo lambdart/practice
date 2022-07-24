64aa35480e012ed26cfcd7bb303310d30133f07a
lambdart <lambdart@protonmail.com>
2022-07-23 23:00:09 -0300

    Add sublist solution

---
+++ clojure/src/sublist.clj
@@ -0,0 +1,15 @@
+(ns sublist)
+
+;; Determine the relation between list1 and list2 and return
+;; the relation as a Clojure keyword. The function classify should either
+;; return :sublist, :superlist, :equal or:unequal.
+(defn classify [list1 list2]
+  (let [[A B result] (if (> (count list1)
+                            (count list2))
+                       [list2 list1 :superlist]
+                       [list1 list2 :sublist])]
+    (cond
+      (= A B) :equal
+      (or (empty? A) (empty? B)) result
+      (= (filter #(contains? B %) A) A) result
+      :else :unequal)))
